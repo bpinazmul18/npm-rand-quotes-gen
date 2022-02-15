50cad31b1beabc45aa3d4fd0391a6aa70457d548
bpinazmul18 <bpinazmul18@gmail.com>
2022-02-15 20:42:24 +0600

    Setup project description and use random number generator.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+/*
+*
+* Title: Basic node app
+* Description: npm random quotes generator is a simple node app.
+* Author: Nazmul Haque
+* Date: 15-02-2022
+*
+*/
+
+
+// Dependency
+const mathLibrary = require('./lib/math')
+
+console.log(mathLibrary.getRandNum(1, 10))
